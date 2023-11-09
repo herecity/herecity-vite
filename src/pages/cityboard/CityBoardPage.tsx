00d253fcfd64d368d7ed5aeeaac8459e868e15c7
JMK1007 <coxld010@naver.com>
2023-11-09 23:05:32 +0900

    feat: 리액트 헬멧 사용해서 메타 태그 추가
    레코드샵, 시티보드 페이지별 메타 태그 추가

--- src/pages/cityboard/CityBoardPage.tsx
+++ src/pages/cityboard/CityBoardPage.tsx
@@ -1,7 +1,23 @@
 import Cityboard from '@components/cityboard/Cityboard';
+import { Helmet } from 'react-helmet';
 
 const CityBoardPage = () => {
-  return <Cityboard />;
+  return (
+    <div>
+      <Helmet>
+        <title>히어시티 | 시티보드</title>
+        <meta property='og:site_name' content='히어시티' />
+        <meta
+          property='og:title'
+          content='엔시티 텍대 모음 #히어시티 #시티보드'
+        />
+        <meta name='description' content='엔시티 텍대 맛집! #히어시티' />
+        <meta property='og:image' content='/assets/images/main_thumbnail.png' />
+        <meta property='og:url' content='https://here-city.com/city-board' />
+      </Helmet>
+      <Cityboard />;
+    </div>
+  );
 };
 
 export default CityBoardPage;
--- src/pages/record/RecordPage.tsx
+++ src/pages/record/RecordPage.tsx
@@ -1,7 +1,26 @@
 import Record from '@components/record/Record';
+import { Helmet } from 'react-helmet';
 
 const RecordPage = () => {
-  return <Record />;
+  return (
+    <div>
+      <Helmet>
+        <title>히어시티 | 레코드샵</title>
+        <meta property='og:site_name' content='히어시티' />
+        <meta
+          property='og:title'
+          content='엔시티 키워드별 플레이리스트 #히어시티 #레코드샵'
+        />
+        <meta
+          name='description'
+          content='엔시티 키워드별 플레이리스트를 간편하게 만들어보새요! #히어시티'
+        />
+        <meta property='og:image' content='/assets/images/main_thumbnail.png' />
+        <meta property='og:url' content='https://here-city.com/record' />
+      </Helmet>
+      <Record />;
+    </div>
+  );
 };
 
 export default RecordPage;
