707a3650527e193db683d31245fea272e2fef5c6
JMK1007 <coxld010@naver.com>
2023-11-22 01:15:53 +0900

    etc
    필요없는  임포트 삭제

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
