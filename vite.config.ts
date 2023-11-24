5e2be5622c289e243911bb46f8ee39dc085402eb
JMK1007 <coxld010@naver.com>
2023-11-24 14:27:29 +0900

    test: vitest.config.ts 추가

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts'],
-  },
   resolve: {
     alias: {
       '@components': '/src/components',
