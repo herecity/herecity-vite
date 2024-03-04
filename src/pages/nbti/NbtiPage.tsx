85a650858afec7ac9dc2f1c29ee57577010a0816
JMK1007 <coxld010@naver.com>
2024-03-04 19:47:44 +0900

    feat: 리액트 헬멧으로 페이지별 메타 태그 추가

--- src/pages/nbti/NbtiPage.tsx
+++ src/pages/nbti/NbtiPage.tsx
@@ -1,7 +1,23 @@
 import Nbti from '@components/nbti/Nbti';
+import { Helmet } from 'react-helmet';
 
 const NbtiPage = () => {
-  return <Nbti />;
+  return (
+    <>
+      <Helmet>
+        <title>히어시티 | NBTI</title>
+        <meta property='og:site_name' content='히어시티' />
+        <meta
+          property='og:title'
+          content='시즈니만의 MBTI 테스트 #히어시티 #NBTI'
+        />
+        <meta name='description' content='시즈니만의 MBTI 테스트! #히어시티' />
+        <meta property='og:image' content='/assets/images/main_thumbnail.png' />
+        <meta property='og:url' content='https://here-city.com/nbti' />
+      </Helmet>
+      <Nbti />;
+    </>
+  );
 };
 
 export default NbtiPage;
