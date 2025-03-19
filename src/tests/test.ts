54dfe7eda12ce43bb2e0ad027cf5b0041bd8a0b8
김지민 <67947887+JMK1007@users.noreply.github.com>
2025-03-19 20:53:08 +0900

    test222 (#16)

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -2,7 +2,7 @@ export const test = () => {
   console.log("123123123");
 };
 export const test2 = () => {
-  console.log("test13234234");
+  console.log("test13dd234234");
 };
 export const test3 = () => {
   console.log("test13234234");
