7876c1f573cd7de77401344f70f83fc482b2503d
JMK1007 <coxld010@naver.com>
2025-04-01 23:23:57 +0900

    222

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -2,7 +2,7 @@ export const test = () => {
   console.log("123123123");
 };
 export const test2 = () => {
-  console.log("test123233dd234234");
+  console.log("222");
 };
 export const test3 = () => {
   console.log("test13234ㄴㅇㄹㄴ234");
