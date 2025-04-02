68aee9cb7feebab67905c573604189fd0a9d6237
JMK1007 <coxld010@naver.com>
2025-04-02 10:52:22 +0900

    22

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -2,7 +2,7 @@ export const test = () => {
   console.log("3451113434345");
 };
 export const test2 = () => {
-  console.log("222");
+  console.log("2222");
 };
 export const test3 = () => {
   console.log("test13333234ㄴㅇㄹㄴ234");
